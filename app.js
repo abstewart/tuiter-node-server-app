65a462f7138f7d3798daf5369e122f1f75564048
abstewart <26328673+abstewart@users.noreply.github.com>
2023-03-29 16:45:45 -0400

    a8 upto 6

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import HelloController from "./controllers/hello-controller.js";
 import UsersController from "./controllers/users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
 const app = express()
 app.use(express.json());
 
 HelloController(app);
 UsersController(app);
+TuitsController(app);
 
 app.listen(4000)

03c7166bb706c53650b720810f7387e2c4ee105f
abstewart <26328673+abstewart@users.noreply.github.com>
2023-03-29 16:08:00 -0400

    a8 upto part 4

---
+++ app.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import HelloController from "./controllers/hello-controller.js";
+import UsersController from "./controllers/users/users-controller.js";
+const app = express()
+
+HelloController(app);
+UsersController(app);
+
+app.listen(4000)

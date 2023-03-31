46de69e1b9fd890017e8114a964261d96540861d
abstewart <26328673+abstewart@users.noreply.github.com>
2023-03-31 16:39:25 -0400

    using mongoose in app and creating tuit schema and model

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from "mongoose";
 import HelloController from "./controllers/hello-controller.js";
 import UsersController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
 const app = express()
 app.use(cors());
 app.use(express.json());
+
+
 
 HelloController(app);
 UsersController(app);
